fcb2d71348c363fa630f7499fa3be1cee48c3d14
Neha Kulkarni <nehakulkarni@Nehas-MacBook-Pro.local>
2021-03-19 13:02:53 -0400

    adding searchAndBuyPhone class file to testng.xml file

---
+++ src/test/java/package1/SearchAndBuyPhone.java
@@ -0,0 +1,21 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class SearchAndBuyPhone {
+
+	@Test
+	public void searchPhone() {
+		System.out.println("In Search Phone test case...");
+	}
+	
+	@Test
+	public void selectPhone() {
+		System.out.println("In select Phone test case...");
+	}
+	
+	@Test
+	public void buyPhone() {
+		System.out.println("In buy Phone test case...");
+	}
+}
