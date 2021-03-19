8dbc2d1429fbeb12dbe07d2860bdef94962deb8d
Neha Kulkarni <nehakulkarni@Nehas-MacBook-Pro.local>
2021-03-19 16:57:37 -0400

    skip tests and dependsOnMethods usuage

--- src/test/java/package1/SearchAndBuyPhone.java
+++ src/test/java/package1/SearchAndBuyPhone.java
@@ -1,5 +1,6 @@
 package package1;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SearchAndBuyPhone {
@@ -7,14 +8,15 @@ public class SearchAndBuyPhone {
 	@Test(priority=1)
 	public void searchPhone() {
 		System.out.println("In Search Phone test case...");
+		Assert.assertEquals(1, 10);
 	}
 	
-	@Test(priority=2)
+	@Test(priority=2, dependsOnMethods="searchPhone")
 	public void selectPhone() {
 		System.out.println("In select Phone test case...");
 	}
 	
-	@Test(priority=3)
+	@Test(priority=3, dependsOnMethods= {"searchPhone", "selectPhone"})
 	public void buyPhone() {
 		System.out.println("In buy Phone test case...");
 	}
