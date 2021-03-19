6d24c036f9904f71a47240386154dafc9e2a5ade
Neha Kulkarni <nehakulkarni@Nehas-MacBook-Pro.local>
2021-03-19 13:18:25 -0400

    adding priority to test cases

--- src/test/java/package1/SearchAndBuyPhone.java
+++ src/test/java/package1/SearchAndBuyPhone.java
@@ -4,17 +4,17 @@ import org.testng.annotations.Test;
 
 public class SearchAndBuyPhone {
 
-	@Test
+	@Test(priority=1)
 	public void searchPhone() {
 		System.out.println("In Search Phone test case...");
 	}
 	
-	@Test
+	@Test(priority=2)
 	public void selectPhone() {
 		System.out.println("In select Phone test case...");
 	}
 	
-	@Test
+	@Test(priority=3)
 	public void buyPhone() {
 		System.out.println("In buy Phone test case...");
 	}
